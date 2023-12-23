68a83ee87ce754e224e2915ebd0c4c72bc58963d
David Northover <david.northover@psu.edu>
2023-12-22 22:06:47 -0500

    Rearrange Normalization
    - Initial floor/ceiling calculation across dataset.
    - Circular Masking before calculation and normalization, if present.

--- transform/transform.py
+++ transform/transform.py
@@ -18,7 +18,7 @@ from shared.cli import FRANGE 	# noqa::E402
 from shared.mem import SharedNP, ProjOrder 	# noqa::E402
 
 
-def norm_helper(image_mem, part, floor, ceiling):
+def normalize(image_mem, part, floor, ceiling):
 	with image_mem[part] as image:
 		image[image > ceiling] = ceiling
 		image[image < floor] = floor
@@ -27,24 +27,17 @@ def norm_helper(image_mem, part, floor, ceiling):
 		image[:, :] /= (ceiling - floor)
 
 
-def normalize(image_mem, index, bottom_threshold, top_threshold, thread_max, slice):
-	""" Straightforward image normalization, disposing of values at edges """
-
+def normalize_calc(image_mem, index, bottom_threshold, top_threshold, slice):
 	with image_mem[index] as image:
 		im_part = (np.s_[:],) + slice
 		log.log("Normalize Setup", f"{image.shape}:{im_part}")
 		floor = np.percentile(image[im_part], bottom_threshold)
 		ceiling = np.percentile(image[im_part], top_threshold)
-
-		log.log('Normalization',
+		log.log('Normalization Setup',
 			f"{np.min(image)}-{np.max(image)}: {bottom_threshold}-{top_threshold} is {floor:.4g}-{ceiling:.4g}",
 			log_level=log.DEBUG.INFO)
 
-		with Pool(thread_max) as pool:
-			pool.starmap(norm_helper, [(image_mem, i, floor, ceiling) for i in index])
-		log.log('Normalization',
-				f"{bottom_threshold} to {top_threshold}: {floor:.4g} to {ceiling:.4g} {(ceiling - floor):.4g}",
-				log_level=log.DEBUG.INFO)
+	return floor, ceiling
 
 
 def convert(source_mem, target_mem, i, j):
@@ -96,8 +89,13 @@ def norm(normalize_over, data_dir, output_dir, vertical_trim, horizontal_trim, o
 	indices = list(range(0, mips_offset + processes))
 
 	Path(output_dir).mkdir(parents=True, exist_ok=True)
-	inputs = [x for x in Path(data_dir).iterdir() if ".tif" in x.name]
-	batched_input = list(batch(sorted(inputs), processes, mips_offset))
+	inputs = sorted([x for x in Path(data_dir).iterdir() if ".tif" in x.name])
+
+	# Because samples vary over rage, we want to normalize across the range.
+	# Interval is calculated so size of normalize dataset is one batch.
+	normalize_interval = len(inputs) / processes
+
+	batched_input = list(batch(inputs), processes, mips_offset)
 
 	log.log("Initialize", "Inputs Batched")
 
@@ -111,10 +109,30 @@ def norm(normalize_over, data_dir, output_dir, vertical_trim, horizontal_trim, o
 	log.log("Initialize", "Tiff Dimensions Fetched")
 
 	with SharedNP('Normalize_Mem', np.float32, mem_shape, create=True) as norm_mem:
+		# Normalization calculation
+		with Pool(processes) as pool:
+			pool.starmap(memreader, [(norm_mem, i, inputs[i]) for i in range(0, len(inputs), len(inputs) % normalize_interval)])
+			log.log("Image Load",
+					f"{len(range(0, len(inputs), len(inputs) % normalize_interval))}|{normalize_interval}"
+					" Images Loaded For Normalization")
+
+			if circ_mask_ratio:
+				with norm_mem as mips_mem:
+					circ_mask(mips_mem, axis=0, ratio=circ_mask_ratio)
+				log.log("Image Masking", f"Images Masked at {circ_mask_ratio}")
+
+			floor, ceiling = normalize(norm_mem, indices[mips_offset:], normalize_over.start, normalize_over.stop, out_dim)
+
 		for input_set in batched_input:
 			with Pool(processes) as pool:
 				pool.starmap(memreader, [(norm_mem, i, input_set[i]) for i in indices if i < len(input_set)])
 			log.log("Image Load", f"{len(indices)}|{len(input_set)} Images Loaded")
+
+			if circ_mask_ratio:
+				with norm_mem as mips_mem:
+					circ_mask(mips_mem, axis=0, ratio=circ_mask_ratio)
+				log.log("Image Masking", f"Images Masked at {circ_mask_ratio}")
+
 			if mips > 1:
 				with norm_mem as mips_mem:
 					for i in reversed(indices[mips_offset:]):
@@ -124,13 +142,16 @@ def norm(normalize_over, data_dir, output_dir, vertical_trim, horizontal_trim, o
 						# log.log("Making MIP", f"{i}:{i - mips_offset}:{mips_mem.shape}:{np.min(mips_mem[i])}:{np.max(mips_mem[i])}",
 						# 			log_level=log.DEBUG.INFO)
 
-			normalize(norm_mem, indices[mips_offset:], normalize_over.start, normalize_over.stop, processes, out_dim)
+			with Pool(processes) as pool:
+				pool.starmap(normalize, [(norm_mem, i, floor, ceiling) for i in input_set])
+
+			log.log('Normalization', f"{floor:.4g} to {ceiling:.4g} {(ceiling - floor):.4g}", log_level=log.DEBUG.INFO)
+
 			# log.log("Image Normalization", f"{len(indices[mips_offset:])} Images Normalized")
-			with norm_mem as mips_mem:
-				circ_mask(mips_mem, axis=0, ratio=circ_mask_ratio)
 			with Pool(processes) as pool:
 				pool.starmap(mem_write, [(norm_mem, Path(output_dir, input_set[i].name), i, out_dim, out_dtype.nptype)
 											for i in indices[mips_offset:]])
+
 			log.log("Image Writing", f"{len(indices[mips_offset:])} Images Written")
 
 
