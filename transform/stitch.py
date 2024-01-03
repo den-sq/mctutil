94248fe7cd7c3a33ed538a53ad70b8ecfc8318c6
David Northover <david.northover@psu.edu>
2024-01-03 13:09:42 -0500

    Switch to handling n samples to ID stitching point from 2.

--- transform/stitch.py
+++ transform/stitch.py
@@ -5,6 +5,19 @@ import numpy as np
 import tifffile as tf
 
 
+class SampleParameter(click.ParamType):
+	name = "Sample Information"
+
+	def convert(self, value, param, ctx):
+		values = value.split(",")
+		proj_folder = Path(values[0])
+		flat_folder = None if len(values) < 2 else values[1]
+		center = None if len(values) < 3 else values[2]
+		tilt = None if len(values) < 4 else values[3]
+
+		return SampleSet(proj_folder, flat_folder, center, tilt)
+
+
 class SampleSet():
 	def __init__(self, proj_folder, flat_folder=None, center=None, tilt=None):
 		self.projs = sorted(list(Path(proj_folder).iterdir()))
@@ -39,39 +52,34 @@ def stitch_proj(samples, overlaps):
 
 
 @click.command()
-@click.option("-f", "--first-sample", type=click.Path(), required=True, help="First stitching sample.")
-@click.option("--first-flat", type=click.Path(), help="Flatfield(s) for first sample.")
-@click.option("--first-center", type=click.FLOAT, help="Rotational center of first image.")
-@click.option("-s", "--second-sample", type=click.Path(), required=True, help="Second stitching sample.")
-@click.option("--second-flat", type=click.Path(), help="Flatfield(s) for second sample.")
-@click.option("--second-center", type=click.FLOAT, help="Rotational center of second image.")
+@click.option("-s", "--sample", type=SampleParameter(), multiple=True,
+				help="Sample information; comma separated list of projection folder, flat folder, rotational center, and tilt.")
 @click.option("--top-stitch/--bottom-stitch", type=click.BOOL, default=False,
 				help="Vertical side of first sample to stitch at.")
 @click.option("--stitch-range", type=click.FLOAT, default=0.2, help="Maximum")
 @click.argument("output_samples")
-def stitch(first_sample, first_flat, first_center, second_sample, second_flat, second_center, top_stitch, stitch_range,
-			output_samples):
-	samples = [SampleSet(first_sample, first_flat, first_center), SampleSet(second_sample, second_flat, second_center)]
-	target_width = min([x.half_width for x in samples])
+def stitch(sample, top_stitch, stitch_range, output_samples):
+	# samples = [SampleSet(first_sample, first_flat, first_center), SampleSet(second_sample, second_flat, second_center)]
+	target_width = min([x.half_width for x in sample])
 
-	for sample in samples:
-		sample.half_width = target_width
-		print(sample.proj.shape)
+	for s in sample:
+		s.half_width = target_width
+		print(s.proj.shape)
 
 	# Stich the samples in reverse order if we're stitching starting at the top; simpler this way.
 	if top_stitch:
-		samples.reverse()
+		sample.reverse()
 
 	overlaps = []
-	stitch_scope = range(20, int(samples[0].proj.shape[0] * stitch_range))
-	for y in range(0, len(samples) - 1):
-		std_set = [(x, np.std(np.divide(samples[y].proj_bot(x), samples[y + 1].proj_top(x)))) for x in stitch_scope]
+	stitch_scope = range(20, int(sample[0].proj.shape[0] * stitch_range))
+	for y in range(0, len(sample) - 1):
+		std_set = [(x, np.std(np.divide(sample[y].proj_bot(x), sample[y + 1].proj_top(x)))) for x in stitch_scope]
 		std_set.sort(key=lambda x: x[1])
 		print(std_set[:10])
 
 		overlaps.append(std_set[0])
 
-	stitch_proj(samples, overlaps)
+	stitch_proj(sample, overlaps)
 
 
 if __name__ == "__main__":
