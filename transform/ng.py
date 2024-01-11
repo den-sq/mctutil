84fadee14e41b04fe1e384a6e7a8a06bac914723
David Northover <david.northover@psu.edu>
2024-01-11 16:16:32 -0500

    Neuroglancer GZIP Handling

--- transform/ng.py
+++ transform/ng.py
@@ -1,3 +1,4 @@
+import gzip
 from pathlib import Path
 
 from neuroglancer_scripts.scripts.generate_scales_info import generate_scales_info
@@ -9,18 +10,22 @@ import click
 
 
 @click.command()
-@click.option('-c', '--chunk-size', type=click.INT, default=128,
+@click.option('-c', '--chunk-size', type=click.INT, show_default=True, default=128,
 				help="Chunk size.  Smaller values faster loads but more files; Larger values have slower but load less files.")
-@click.option('-r', '--resolution', type=click.INT, default=1400, help="Dataset Resolution, in nanometers")
-@click.option('--segmentation/--raw_data', type=click.BOOL, default=False,
+@click.option('-r', '--resolution', type=click.INT, show_default=True, default=1400,
+				help="Dataset Resolution, in nanometers")
+@click.option('--segmentation/--raw_data', type=click.BOOL, show_default=True, default=False,
 				help="Whether we are writing segmentations or raw image data.")
 @click.option('-i', '--input-path', type=click.Path(), required=True, help="Path to input files.")
 @click.option('-m', '--metadata-info', type=click.Path(dir_okay=False, writable=True), required=True,
-				help="Location for the Neuroglancer Metadata Info File.")
-@click.option("--strip-gz/--keep-gz", type=click.BOOL, default=False,
+				help="(Temporary) Location for the Neuroglancer Metadata Info File.")
+@click.option("--strip-gz/--keep-gz", type=click.BOOL, show_default=True, default=False,
 				help="Whether to strip gz extennsions from filenames, as they can confuse neuroglancer.")
-@click.option('-o', '--output-location', type=click.STRING, required=True)
-def neuroglance(chunk_size, resolution, segmentation, strip_gz, input_path, metadata_info, output_location):
+@click.option("--compress-info", type=click.BOOL, is_flag=True, show_default=True, default=False,
+				help="Whether to compress info file as gz as well (without changing extension).")
+@click.option('-o', '--output-location', type=click.Path(), required=True)
+def neuroglance(chunk_size, resolution, segmentation, strip_gz, input_path, metadata_info, compress_info,
+				output_location):
 	print(f'input folder: {input_path}')
 	image_paths = list(Path(input_path).glob("**/*.tif"))
 
@@ -73,6 +78,12 @@ def neuroglance(chunk_size, resolution, segmentation, strip_gz, input_path, meta
 	if strip_gz:
 		for out_file in Path(output_location).glob("**/*.gz"):
 			out_file.rename(out_file.with_suffix(""))
+
+	if compress_info:
+		with open(Path(output_location, "info"), 'rb') as handle:
+			info_data = handle.read()
+		with gzip.open(Path(output_location, "info"), 'wb') as gzip_out:
+			gzip_out.write(info_data)
 
 
 if __name__ == '__main__':
